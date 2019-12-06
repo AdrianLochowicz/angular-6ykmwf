1999624732ef82a79ca7f52b82fb75062308fc7a
AdrianLochowicz <la36856@zut.edu.pl>
2019-12-06 17:45:34 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   name = 'Angular 5';
 
-  komunikat:string;
+  komunikat: string;
 nowyKomunikat: Komunikat;
 
   ngOnInit() {
