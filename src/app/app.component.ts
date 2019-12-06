95b0ae755ea3e90e0656fe217cd8eeded2e573dd
AdrianLochowicz <la36856@zut.edu.pl>
2019-12-06 16:26:46 +0100

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  templateUrl: './hello.component.html',
+  styleUrls: [ './hello.component.css' ]
 })
 export class AppComponent  {
   name = 'Angular';
