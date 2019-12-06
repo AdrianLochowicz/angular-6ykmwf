db036ccd8fad48d0cbb60276d069fc3d55563f4f
AdrianLochowicz <la36856@zut.edu.pl>
2019-12-06 17:44:36 +0100

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,15 @@ export class AppComponent implements OnInit {
   name = 'Angular 5';
 
   komunikat:string;
+nowyKomunikat: Komunikat;
 
- 
   ngOnInit() {
     this.komunikat = [
   'Ala ma kota',
   'Kot ma dosc Ali',
   'Kot i Ala drą ze sobą koty'
 ];
+this.nowyKomunikat = new Komunikat('Nowy komunikat', new Date(), true);
   }
 }
-}
+import { Komunikat } from './komunikat';
