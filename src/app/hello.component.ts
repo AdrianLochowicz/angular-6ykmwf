1dae981afebd7e70ed2964c2a1926240e1b097bd
aksyzn <la36856>
2019-12-09 17:42:45 +0100

    test

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
   styleUrls: ['./hello.component.css'],
 })
-export class HelloComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class HelloComponent  {
+  @Input() name: string;
 
 }
