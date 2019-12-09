6fe358d1df4e797da0924ecd56a295a34814496d
aksyzn <la36856>
2019-12-09 16:24:45 +0100

    test

--- src/komunikat/komunikat.component.ts
+++ src/komunikat/komunikat.component.ts
@@ -6,13 +6,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./komunikat.component.css']
 })
 export class KomunikatComponent implements OnInit {
-<<<<<<< HEAD
 
   @Input() komunikaciki: any[];
-=======
-name = 'lab3';
-@input() komunikcik: any[];
->>>>>>> ff92a2cefc2798116f220b7b42008ba69d637e0a
 
   constructor() { }
 
